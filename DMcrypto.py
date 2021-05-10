95d2e515fd97de306e8e2e7787ab7033836038cf
uvsq22004090 <71641885+uvsq22004090@users.noreply.github.com>
2021-05-10 09:48:33 +0200

    Add files via upload

---
+++ DMcrypto.py
@@ -0,0 +1,134 @@
+'''
+Texte 1:
+
+kd oqnbgzhm ehbghdq ztqz tm bncd ozq rtarshstshnm zkogzadshptd: bgzptd kdssqd drs qdlokzbdd ozq tmd ztsqd. tshkhrdq kz eqdptdmbd cdr kdssqdr ontq cdbncdq kd ldrrzfd.
+
+clef 1:
+Z, ou ZZ, ou ZZZ.....
+
+Texte dechiffre 1: 
+
+le prochain fichier aura un code par substitution alphabetique: chaque lettre est remplacee par une autre. utiliser la frequence des lettres pour decoder le message.
+
+Texte 2:
+
+gx qosvlnkd wkvlkxo xiu vscx qno yd fsu cx qniix cx unkggx kdvsddyx xu vsdukxdu g'kdckvx. gxi gxuuoxi cy fsu cx qniix qxofxuuxdu cx cxvngxo gxi gxuuoxi cy fxiinmx sokmkdng fscygs 26. ixygxi gxi gxuuoxi cx n n a isdu vlkwwoxxi.\
+
+Clef 2: y'en a pas
+
+Texte dechiffre 2:
+
+LE PROCHAIN FICHIER EST CODE PAR UN MOT DE PASSE DE TAILLE INCONNUE ET CONTIENT L'INDICE. LES LETTRES DU MOT DE PASSE PERMETTENT DE DECALER LES LETTRES DU MESSAGE ORIGINAL MODULO 26. SEULES LES LETTRES DE A A Z SONT CHIFFREES. 
+
+Texte 3:
+
+dceuq e n'ehfp cg p'kyhhep uqfw cgiy citudm c gzudiq ni ezhd px c jhptv ep cggsht. kg hdtymdt xdzei gdx rzyq wir mvzxpw, cifcchdb znwd ccyw wy lkcsht, dp isgd uqfw wy ?
+
+Clef 3:
+
+lettre 1 = c
+lettre 2 = l
+lettre 3 = e
+lettre 4 = z
+
+Texte dechiffre 2: 
+
+BRAVO A L AIDE DE L INDICE VOUS AVEZ REUSSI A CASSER CE CODE ET A FINIR CE DEVOIR  LE DERNIER TEXTE EST POUR LES BRAVES  REGARDEZ VOUS DANS UN MIROIR  EN ETES VOUS UN ?
+'''
+
+
+#permet de tester des clefs plus facilement 
+
+code_chiffre = str(input("entrez le code a modifier:"))
+clef = str(input("entrez une clef de dechiffrage:"))
+
+
+
+#code qui deplace la valeur ascii d'un message et retourne le message associe aux nouvelles valeurs ascii.
+def code_vigenere1 ( message, clef) :
+    
+    #le message code sera en forme de liste
+    message_code = []
+
+
+    for i,c in enumerate(message) :
+
+
+        #decalage ascii
+        d = clef[ i % len(clef) ]
+        d = ord(d) - 65
+        d = 26 - d
+
+        #pour eviter que les ".", les espaces et les ":" soient traduits, je ne prends que les caracteres avec une valeur ascii superieur a 65,
+        #  ce qui comprends les lettres de A a Z + d'autres caracteres bizares qui ne sont pas dans les messages. 
+        if ord(c) <= 65:
+            
+            correspondant = " " 
+            
+        else:
+           correspondant = chr((ord(c)-65+d)%26 + 65) 
+       
+        #append les lettres dechiffres dans la liste
+        message_code += correspondant
+
+        #permet d'avoir un message lisible et non une liste
+        message_code = "".join(message_code)
+    return message_code
+
+
+#foction qui permet de decouper le texte en un nomnre de segments donne
+def decouper(texte,Nombre_De_Segments):
+    #les segments seronts stoques sous forme de listes
+    global segments
+    segments = []
+    #la taille est definie par la division de la taille du texte par le nb de segments
+    tailleSegment = int(len(texte)/Nombre_De_Segments)
+    #prints de verifications
+    print(len(texte))
+    print(tailleSegment)
+    #decoupage et stockage des segents
+    for i in range(0,Nombre_De_Segments):
+        segments.append(texte[i * tailleSegment  :i * tailleSegment + tailleSegment])
+    #permet de se rajouter le dernier caractere a la derniere liste si la phrase n'est pas divisible sans reste
+    if len(texte) % Nombre_De_Segments :
+        segments[-1] = segments[-1] + texte[-1]
+    #prints de verifications
+    for i in range(len(segments)):
+        print(len(segments[i]))
+    print(segments)
+    
+#fonction permetant de calculer la frequence de chaque caractere dans un texte donne
+def frequence(code_chiffre):
+    #on echappe pas a la regle, tout est stocke en listes
+    resultat = []
+    resultatTrie = []
+    listeFreq = []
+    nbLettres = 0
+    #calcul du nombre de lettres dans le texte
+    for c in code_chiffre:
+        if 97 <= ord(c) < 123:
+            nbLettres += 1
+    #cherche les caracteres du texte et stock leurs valeurs dans resultat
+    for i in range(97, 123):
+        if chr(i) in code_chiffre:
+            resultat.append((chr(i), round(code_chiffre.count(chr(i))/nbLettres*100,2)))
+    #prend les frequences d'apparition et les stockent dans listeFreq
+    for i in resultat:
+        listeFreq.append(i[1])
+    #trie les resultats du plus frequent au moins frequent
+    while(len(listeFreq)):
+        indexMax = listeFreq.index(max(listeFreq))
+        resultatTrie.append(resultat[indexMax])
+        resultat.remove(resultat[indexMax])
+        listeFreq.remove(listeFreq[indexMax])
+    return resultatTrie
+
+
+#prints
+print(decouper(code_chiffre, 38))
+print(frequence(code_chiffre))
+print(code_vigenere1(code_chiffre, clef))
+print(frequence(segments[0]))
+print(frequence(segments[1]))
+print(frequence(segments[2]))
+print(frequence(segments[3]))
